6795988c304d9681fad7d6c4024806f1e2856d19
rahuljha12527 <45179877+rahuljha12527@users.noreply.github.com>
2019-06-15 20:43:59 +0530

    Update Day14.cpp

--- Day14.cpp
+++ Day14.cpp
@@ -1,1 +1,48 @@
-#include<>
+#include<cmath>
+#include<cstdio>
+#include<vector>
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+
+class Difference{
+private:
+	vector<int> elements;
+
+public:
+	int maximumDifference;
+
+	Difference(std::vector<int> a){
+		elements=a;
+		maximumDifference=0;	
+	}
+	void computeDifference(){
+		int n=elements.size()-1;
+		for(int i=0;i<n;i++){
+			for(int j=i;j<n;j++){
+				if(maximumDifference<abs(elements[i]-elements[j+1])){
+					maximumDifference=abs(elements[i]-elements[j+1]);
+				}
+			}
+		}
+	}
+};
+
+
+int main(){
+	int N;
+	cin>>N;
+	std::vector<int> v;
+	for(int i=0;i<N;i++){
+		int e;
+		cin>>e;
+		v.push_back(e);
+	}
+
+	Difference d(v);
+	d.computeDifference();
+	cout<<d.maximumDifference;
+
+	return 0;
+}
